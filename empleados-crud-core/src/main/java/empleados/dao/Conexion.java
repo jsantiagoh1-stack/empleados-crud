c85e9e08a8f242e2eb5a2b83838bd5b1f6f3438d
JOSÉ JULIÁN SANTIAGO HERNÁNDEZ <jsantiagoh1@miumg.edu.gt>
2026-09-17 21:01:32 -0600

    feat: agregar clase Conexion para MySQL

--- empleados-crud-core/src/main/java/empleados/dao/Conexion.java
+++ empleados-crud-core/src/main/java/empleados/dao/Conexion.java
@@ -1,5 +1,15 @@
 package empleados.dao;
 
-public class Conexion {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Conexion {
+    private static final String URL = "jdbc:mysql://localhost:3306/empresa_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = ""; // Coloca tu contraseña de MySQL aquí si tienes una
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
